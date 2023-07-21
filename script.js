c78e17d00c133425f6499a827702660fa56704b5
Tiago Rodrigues <rodriguessouzatg@gmail.com>
2023-07-21 15:11:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const key = '87a5d1f50d5a93aac995a1fc0384effd'
+
+function colocarDadosNaTela(dados){
+  document.querySelector('.cidade').innerHTML = 'Tempo em ' + dados.name
+  document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + 'ºC'
+  document.querySelector('.texto-previsao').innerHTML = dados.weather[0].description
+  document.querySelector('.umidade').innerHTML = 'Umidade do ar: ' + dados.main.humidity + '%'
+  document.querySelector('.img-previsao').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+   colocarDadosNaTela(dados)
+}
+
+function clickInButton() {
+
+  const cidade = document.querySelector('.input-cidade').value
+
+  buscarCidade(cidade)
+
+}
